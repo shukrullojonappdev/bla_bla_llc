6498e94cdd30507a0e425141c2edc3aa2dcb403e
shukrullojonappdev <shukrullojon.dev@gmail.com>
2024-08-15 00:08:00 +0500

    commited

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -29,12 +29,21 @@ export default function Footer() {
             <div className="col-span-1 grid">
               {/* 2nd block */}
               <div className="text-sm">
-                <h6 className="text-gray-200 font-medium mb-1">Address</h6>
-                <span className="text-gray-400">
+                <h6 className="text-gray-200 font-medium mb-1 text-xl">
+                  Address
+                </h6>
+                <span className="text-gray-400 text-lg">
                   175 SW 7TH st STE 1517-1057 Miami Florida 33130
                 </span>
-                <h6 className="text-gray-200 font-medium mb-1 mt-4">Phone</h6>
-                <span className="text-gray-400">+1-754-275-1401</span>
+                <h6 className="text-gray-200 font-medium mb-1 mt-4 text-xl">
+                  For more info, please contact to
+                </h6>
+                <a
+                  href="tel:+17542751401"
+                  className="text-gray-400 text-lg hover:text-gray-200 duration-300"
+                >
+                  +1-754-275-1401
+                </a>
               </div>
             </div>
           </div>
@@ -58,7 +67,7 @@ export default function Footer() {
                   </svg>
                 </Link>
               </li>
-              <li className="ml-4">
+              {/* <li className="ml-4">
                 <Link
                   href="/"
                   className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
@@ -72,7 +81,7 @@ export default function Footer() {
                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                   </svg>
                 </Link>
-              </li>
+              </li> */}
               <li className="ml-4">
                 <Link
                   href="/"
